5329cef17771ac6b507541c621d9e3afe1169ff1
rockaman3-netizen <rock.aman3@gmail.com>
2026-09-25 23:24:26 +0530

    Update capacitor.config.customer.ts

--- capacitor.config.customer.ts
+++ capacitor.config.customer.ts
@@ -2,11 +2,14 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.carrycub.customer',
-  appName: 'CarryCub Customer',
+  appName: 'CarryCub',
   webDir: 'www',
   server: {
     url: 'https://carrycub-booking-fb.rock-aman3.workers.dev/',
     cleartext: false
+  },
+  android: {
+    path: 'android-customer'
   }
 };
 
